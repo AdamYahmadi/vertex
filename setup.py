011419144321b1bb111f852bd6dfb6f6d86e81eb
AdamYahmadi <adem.yahmadi@tum.de>
2026-06-14 18:36:45 +0200

    fix

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="vertex",
     version="0.1",
-    packages=find_packages(), # This will find 'src'
-    py_modules=["main"],       # This tells it to find 'main.py' in the root
+    packages=find_packages(),
+    py_modules=["main"],       
     entry_points={
         'console_scripts': [
             'vertex=main:main',
